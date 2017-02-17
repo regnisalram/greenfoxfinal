571feb3a28cb6d404f62a0374027dc528f03e488
regnisalram <reka.bence@gmail.com>
2017-02-17 09:40:48 +0100

    of course i messed up the name of the file

--- src/Task2.java
+++ src/Task2.java
@@ -2,7 +2,7 @@
 /**
  * Created by regnisalram on 2/17/17.
  */
-public class Task4 {
+public class Task2 {
 
     public static void main(String[] args) {
         System.out.println(reverseString("Green Fox"));
